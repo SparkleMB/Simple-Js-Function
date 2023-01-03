97d8e3c847ac2b6a6214394a862dec5eb3fbb2e5
SparkleMB <71784800+SparkleMB@users.noreply.github.com>
2023-01-03 17:58:50 -0500

    Inputed info

---
+++ index.js
@@ -0,0 +1,17 @@
+function myInfo(info) {
+    console.log(`My name is ${info}`);
+}
+
+myInfo("Sparkle");
+
+function myCountry(country) {
+    console.log(`I live in ${country}`);
+}
+
+myCountry("USA");
+
+function myHeight(height) {
+    console.log(`My height is ${height}`);
+}
+
+myInfo("5'2");
