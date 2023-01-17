67c3286927dff3a69f8d45a3ceb5956651087800
SparkleMB <71784800+SparkleMB@users.noreply.github.com>
2023-01-17 12:24:22 -0500

    Update index.js
    Removed console.log

--- index.js
+++ index.js
@@ -1,18 +1,6 @@
 function myInfo(info,country, height) {
-    console.log('My name is ${info}, ${country}, ${height}');
-    alert("My name is" +" " +info + " "+"I live in "+country+" "+ "I am" + height);
+    ('My name is ${info}, ${country}, ${height}');
+    alert("My name is" +" " +info + " "+"I live in "+country+" "+ "I am " + height);
 }
 
-myInfo("Sparkle");
-
-function myCountry(country) {
-    console.log(`I live in ${country}`);
-}
-
-myCountry("USA");
-
-function myHeight(height) {
-    console.log(`My height is ${height}`);
-}
-
-myInfo("5'2");
+myInfo('Sparkle', 'USA', "5'2");
